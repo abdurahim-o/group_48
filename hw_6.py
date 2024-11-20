169876375578a19b94b5565b0a0510cc6b2c286c
abdurahim-o <abdurahimmobile22@gmail.com>
2024-11-20 20:45:06 +0300

    изменил кодÐ

--- hw_6.py
+++ hw_6.py
@@ -1,11 +1,35 @@
-def bubble_sort(arr):
-    n = len(arr)
+def bubble_sort(cristiano):
+    n = len(cristiano)
     for i in range(n):
+        for i in range(0, n-i-1):
+            if cristiano[i] > cristiano[i+1]:
+                cristiano[i], cristiano[i+1] = cristiano[i+1], cristiano[i]
 
-                for j in range(0, n-i-1):
-            if arr[i] > arr[i+1]:
-                arr[i], arr[i+1] = arr[i+1]
+    return cristiano
+
+sorted_list = [7, 9, 10, 11, 8]
+sorted_list = bubble_sort(sorted_list)
+print("Cписок:", sorted_list)
 
 
-unsorted_list = [7, 9, 28, 8, 10, 13, 5]
-print("Список:",)
+def binary_search(cristiano, x):
+    left, right = 0, len(cristiano) - 1
+
+    while left <= right:
+        mid = left + (right - left)//2
+
+        if cristiano[mid] == x:
+            print(f"Нашел {x} на строчке {mid}")
+            return
+
+        elif cristiano[mid] < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    print(f"{x} не найден")
+    return
+
+
+element_to_find = 7
+binary_search(sorted_list, element_to_find)
